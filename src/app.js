62b918c58e4e2a53d2f3fbc7a068a76281df9666
SophieMps <sophiebasiaridou@gmail.com>
2022-12-11 14:52:13 +0200

    Added current date & time on JS

---
+++ src/app.js
@@ -0,0 +1,37 @@
+function liveDayTime() {
+  let now = new Date();
+
+  let date = now.getDate();
+  let hour = now.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let year = now.getFullYear();
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[now.getDay()];
+
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[now.getMonth()];
+
+  let showCurrentDay = document.querySelector("#current-day");
+  showCurrentDay.innerHTML = `${day}, ${date} ${month} ${year} | ${hour}:${minutes}`;
+}
+liveDayTime();
