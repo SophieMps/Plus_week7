851218de1d79ba5df2326154bf4665be04e118d2
SophieMps <sophiebasiaridou@gmail.com>
2022-12-24 14:41:43 +0200

    Add forecast into JS

--- src/app.js
+++ src/app.js
@@ -49,7 +49,20 @@ liveDayTime();
 
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = "Forecast";
+  forecastElement.innerHTML = `
+  <div class="weather-forecast" id="forecast">
+            <div class="row">
+              <div class="col-2">
+                <div class="weather-forecast-date">Thu</div>
+                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="" width="42"/>
+                <div class="weather-forecast-temperature">
+                  <span clas="weather-forecast-temperature-max">18°</span>
+                  <span class="weather-forecast-temperature-min">12°</span>
+                </div>
+              </div>
+            </div>
+          </div>
+  `;
 }
 
 function displayTemp(response) {
