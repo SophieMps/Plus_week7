478d81f245808bc1fa5424c630d4f78619e7f9de
SophieMps <sophiebasiaridou@gmail.com>
2022-12-24 17:49:46 +0200

    delete console logs

--- src/app.js
+++ src/app.js
@@ -73,7 +73,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "12bdbd86bcab685847fea4f4c4d743cf";
   let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
