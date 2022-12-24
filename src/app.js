2a41586b01cc0466d550928e3161109ad8e4e029
SophieMps <sophiebasiaridou@gmail.com>
2022-12-24 17:10:37 +0200

    Add API One call

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ function liveDayTime() {
 
 liveDayTime();
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -72,6 +72,9 @@ function displayForecast() {
 
 function getForecast(coordinates) {
   console.log(coordinates);
+  let apiKey = "12bdbd86bcab685847fea4f4c4d743cf";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemp(response) {
