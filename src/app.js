b39ea53fe7479b8ebd6f4aebeb22ab9bf651ec8f
SophieMps <sophiebasiaridou@gmail.com>
2022-12-24 18:05:37 +0200

    Fine tunning in mistakes

--- src/app.js
+++ src/app.js
@@ -47,6 +47,17 @@ function liveDayTime() {
 
 liveDayTime();
 
+function formatDay(timestamp) {
+let date= new Date(timestamp * 1000); 
+let day= getDay ();
+let days= ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+
+
+return days[day];
+}
+
+
 function displayForecast(response) {
   let forecast = response.data.daily;
 
@@ -58,13 +69,13 @@ function displayForecast(response) {
       forecastHTML +
       ` 
                           <div class="col-2">
-                <div class="weather-forecast-date">${forecastDay.dt}</div>
+                <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
                 <img src=`http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png` alt="" width="42"/>
                 <div class="weather-forecast-temperature">
                   <span clas="weather-forecast-temperature-max">${forecastDay.temp.max}°</span>
                   <span class="weather-forecast-temperature-min">${forecastDay.temp.min}°</span>
                 </div>
-              </div>
+              </ div>
          
   `;
   });
